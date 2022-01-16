0e7875071239f71f3c5f55f33038fa94eb0494c1
Aaron <afosoy.uni@gmail.com>
2022-01-16 10:26:57 -0800

    Partially implemented renderMatrix to put matrix in row echelon form; TODO: debugging of renderMatrix fn

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { useState } from "react";
 
 function App() {
   //setting fields
-  const [size, setSize] = useState(3);
-  var matrix = new Array();
+  const [matSize, setSize] = useState(3);
+  var matrix = [];
 
   // initialize matrix input
-  const rows = new Array();
-  const renderedRows = new Array();
+  const rows = [];
+  var renderedRows = [];
 
   // Configures input for a matrix (2d Array)
-  for (let i = 0; i < size; i++) {
-    var matrixCol = new Array();
-    for (let j = 0; j <= size; j++) {
+  for (let i = 0; i < matSize; i++) {
+    var matrixCol = [];
+    for (let j = 0; j <= matSize; j++) {
       var entryId = String(i) + String(j);
       matrixCol.push(
         <span>
@@ -23,39 +23,75 @@ function App() {
     rows.push(<div>{matrixCol}</div>);
   }
 
-  function makeMatrix() {
-    for (let i = 0; i < size; i++) {
-      var matrixCol = new Array();
-      for (let j = 0; j <= size; j++) {
+  // Converts user matrix input into array
+  function makeMatrixArr() {
+    for (let i = 0; i < matSize; i++) {
+      var matrixCol = [];
+      for (let j = 0; j <= matSize; j++) {
         var entryId = String(i) + String(j);
-        var matrEntry = document.getElementById(entryId).value;
-        matrixCol.push(parseInt(matrEntry));
+        var matEntry = document.getElementById(entryId).value;
+        matrixCol.push(parseInt(matEntry));
       }
       matrix.push(matrixCol);
     }
-    renderedRows = renderMatrix(matrix);
+    renderMatrix();
     console.log(matrix);
   }
 
   //TODO: render new matrix in reduced form
-  function renderMatrix(arr) {
-    return new Array();
+  function renderMatrix() {
+    for (let i = 0; i < matSize; i++) {
+      // find the largest row index to use for pivot
+      var max_row = i;
+      var max_pivot = matrix[max_row][i];
+
+      for (let j = i + 1; j < matSize; j++) {
+        if (Math.abs(matrix[j][i]) > max_pivot) {
+          max_row = j;
+          max_pivot = matrix[j][i];
+        }
+      }
+
+      // a pivot col with all entries being 0 indicates a free variable and we skip over it
+      if (max_pivot == 0) {
+        continue;
+      }
+
+      // swap the current row with the row with the largest pivot
+      if (max_pivot != i) {
+        var temp = matrix[i];
+        matrix[i] = matrix[max_row];
+        matrix[max_row] = temp;
+      }
+
+      // go through each row, perform row operations to have all entries below pivot have a 0 entry
+      for (let k = i + 2; k < matSize; k++) {
+        // find ratio that reduces col entry to 0
+        var ratio = matrix[k][i] / parseFloat(matrix[i][i]);
+
+        // subtract ratio multiple of corresponding column element from curr i in curr k
+        for (let currCol = i + 2; currCol <= matSize; k++) {
+          matrix[k][currCol] -= ratio * matrix[i][currCol];
+        }
+      }
+      console.log(matrix);
+      return matrix;
+    }
   }
 
   return (
     <div>
       <div className="ui raised segment">
         <h1 className="header">AM NXN Augmented Matrix Reducer</h1>
-      </div>
-      <div className="ui raised segment">
         <h4 className="title-caption">
           because no one has time to reduce large matrices ;D
         </h4>
+      </div>
+      <div className="ui raised segment">
         <p className="how-to">
           Don't want to spend the time reducing matrices yourself? Use this tool
-          to reduce your matrix for you! Simply input the size "n"{" "}
-          to display an NxN matrix
-          and fill in the entries!
+          to reduce your matrix for you! Simply input the size "n" to display an
+          NxN matrix and fill in the entries!
           <br></br>
           (Sorry, bootstrap doesn't allow sizing past 23{" "}
           <span class="bold">O_O</span>)
@@ -73,16 +109,16 @@ function App() {
             ></input>
           </form>
         </div>
-      <div className="container matrix">
-        {rows}
-        <input
-          className="btn btn-dark btn-xl changeSizeButton reduce-margin"
-          value="Reduce Matrix!"
-          onClick={makeMatrix}
-        ></input>
+        <div className="container matrix">
+          {rows}
+          <input
+            className="btn btn-dark btn-xl changeSizeButton reduce-margin"
+            value="Reduce Matrix!"
+            onClick={makeMatrixArr}
+          ></input>
+        </div>
       </div>
-      </div>
-      
+
       <div className="container reduced-matrix">{renderedRows}</div>
     </div>
   );
