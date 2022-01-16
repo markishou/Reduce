49d73e0c3bc472ece4d44383214750b2d85cbbb6
markishou <myhou0720@gmail.com>
2022-01-15 20:02:17 -0800

    first commit/matrix size input

--- src/gridInput.js
+++ src/gridInput.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const GridInput = ({size}) => {
+    // const renderedTodos = props.todos.map((todo) => {
+    //     return <Todo todo={todo} key={todo.id}/>
+    // });
 
+    
 
     return (
         <div>
